8d4bd3494e431df03abb5225440a41d68f7c35f9
PhilippeBaumstimler <philippe.baumstimler@ensta-paris.fr>
2022-01-19 11:01:02 +0100

    scene dobjets

--- objects.hpp
+++ objects.hpp
@@ -2,14 +2,13 @@
 # define OBJECTS_HPP
 
 # include <iostream>
-# include <stdbool.h>
 # include <vector>
 # include <math.h>
 # include "vec.hpp"
 
 
 
-//Structure contenant le point d'intersection entre un objet et la normal au point de collision
+//Structure contenant le point d'intersection entre un objet et un rayon, et la normal au point de collision
 struct hit_position{
     point hit_point;
     vec normal;
@@ -21,7 +20,7 @@ class scene_basic_object{
     public:
         scene_basic_object(){}
         ~scene_basic_object(){}
-        virtual struct hit_position hit_object(ray r)=0;
+        virtual double hit_object(ray r,struct hit_position* intersect)=0;
 
 };
 
@@ -32,13 +31,20 @@ class sphere: public scene_basic_object{
         double radius;
     public:
         sphere(){}
-        sphere(point aCenter, double aRadius) : center(aCenter), radius(aRadius){}
-        sphere(sphere aSphere) : center(aSphere.center), radius(aSphere.radius){}
+        sphere(point aCenter, double aRadius) : scene_basic_object() {center = aCenter; radius = aRadius;}
+        sphere(const sphere& aSphere) : center(aSphere.center), radius(aSphere.radius){}
         ~sphere(){}
-        double hit_object(ray r, struct hit_point* intersect){
+        point getCenter() const{
+            return center;
+        }
+        double getRadius() const{
+            return radius;
+        }
+        //Renvoi step, qui est la distance entre le point d'origine du rayon et le point d'impact
+        double hit_object(ray r, struct hit_position* intersect){
             double A = r.getDirection()*r.getDirection();
-            double B = 2.0*r.getDirection()*(r.getOrigin()-center);
-            double C = (r.getOrigin()-center)*(r.getOrigin-center) - radius*radius;
+            double B = r.getDirection()*(r.getOrigin()-center)*2.0;
+            double C = (r.getOrigin()-center)*(r.getOrigin()-center) - radius*radius;
             double delta = B*B - 4*A*C;
             if(delta >= 0){
                 double step = (-B-sqrt(delta))/(2*A);
@@ -59,7 +65,8 @@ class ground: public scene_basic_object{
         ground(){}
         ground(point anOrigin, vec aNormal) : origin(anOrigin), normal(aNormal){}
         ~ground(){}
-        double hit_position hit_object(ray r, struct hit_point* intersect){
+        //Renvoi step, qui est la distance entre le point d'origine du rayon et le point d'impact
+        double hit_object(ray r, struct hit_position* intersect){
             double delta = normal*r.getDirection();
             if(delta!=0){
                 double num = normal*(origin - r.getOrigin());
@@ -72,15 +79,39 @@ class ground: public scene_basic_object{
         };
 };
  
-//Classe regroupant l'ensemble des objets présent sur la scène
+//Classe regroupant l'ensemble des objets présent sur scène
 class scene{
     private:
         std::vector<scene_basic_object*> list_objects;
     public:
         scene(){}
-        scene(vector::std<scene_basic_object*> aList_objects) : list_objects(aList_objects){}
-        scene(scene aScene) : list_objects(aScene.list_objects){}
+        scene(std::vector<scene_basic_object*> aList_objects) : list_objects(aList_objects){}
+        scene(const scene &aScene) : list_objects(aScene.list_objects){}
         ~scene(){}
+        std::vector<scene_basic_object*> getList(){
+            return list_objects;
+        }
+        void add(scene_basic_object* new_object){
+            list_objects.push_back(new_object);
+        }
+        void clear(){
+            list_objects.clear();
+        }
+        double hit_list(ray r, struct hit_position* intersect, double max_step){
+            hit_position tmp;
+            double step = max_step;
+            for(int i=0; i<(int)list_objects.size();i++){
+                double res = list_objects[i]->hit_object(r,&tmp);
+                if(res!=-1.0 && res < step){
+                    *intersect = tmp;
+                    step = res;
+                }
+            }
+            if(step == max_step){
+                return -1.0;
+            }
+            return step;
+        }
 };
 
 # endif 
--- vec.hpp
+++ vec.hpp
@@ -1,6 +1,7 @@
 #ifndef CLASSES_HPP
 #define CLASSES_HPP
 #include <iostream>
+#include <ostream>
 #include <cmath>
 
 //Classe générale pour un triplet
@@ -86,17 +87,17 @@ class triple{
 
 //Création d'alias sur les triplets pour bien définir les objets utilisés dans le programme
 //Décrit les couleurs en RGB
-typedef triple<int> color;
+typedef triple<double> color;
 //Décrit un point (x,y,z) dans l'espace
 typedef triple<double> point;
 //Décrit un vecteur (x,y,z) dans l'espace
 typedef triple<double> vec;
 
 //Surcharge flux de données en écriture pour afficher les triplets
-template<class charT, class charTraits, typename T>
-std::basic_ostream<charT>& operator <<(std::basic_ostream<charT, charTraits>& theStream, const triple<T>& aTriple)
+template<typename T>
+std::ostream& operator <<(std::ostream& theStream, const triple<T>& aTriple)
 {
-    theStream << "(" << aTriple.getX() << "," << aTriple.getY() << "," << aTriple.getZ() << ")" << std::endl; 
+    theStream << "(" << aTriple.getX() << "," << aTriple.getY() << "," << aTriple.getZ() << ")"; 
     return theStream;
 }
 
